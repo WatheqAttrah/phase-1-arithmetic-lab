02346a83dee53683e75252ceb41fd3871d7dd6e9
Watheq Attrah <novaatrah@gmail.com>
2023-03-17 17:24:41 -0700

    Lab Complete

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 *num2;
+console.log(multiply);
+
+let random = 1;
+console.log(Math.random(1));
+
+let num3 = 24;
+let num4 = 10;
+let mod = num3 % num4;
+console.log(mod);
+
+
+let max = Math.max(10,19,1,20,13,14);
+console.log(max);
